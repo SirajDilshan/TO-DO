c584aeb2f2c86e103e7ff2abe4de7cd71262af23
SirajDilshan <sirajdilshan789@gmail.com>
2025-01-31 23:49:03 +0530

    create ToDoRepository interface

--- src/main/java/SpringBoot/TO_DO/repository/ToDoRepository.java
+++ src/main/java/SpringBoot/TO_DO/repository/ToDoRepository.java
@@ -1,4 +1,10 @@
 package SpringBoot.TO_DO.repository;
 
-public class ToDoRepository {
+import SpringBoot.TO_DO.entity.ToDoEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ToDoRepository extends JpaRepository<ToDoEntity, Long> {
+
 }
