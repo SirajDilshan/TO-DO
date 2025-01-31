27beb0b71e8510e68053d0ba598c599881f37be2
SirajDilshan <sirajdilshan789@gmail.com>
2025-01-31 23:39:21 +0530

    create the ToDOEntity Class

--- src/main/java/SpringBoot/TO_DO/entity/ToDoEntity.java
+++ src/main/java/SpringBoot/TO_DO/entity/ToDoEntity.java
@@ -1,4 +1,22 @@
 package SpringBoot.TO_DO.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name="todos")
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class ToDoEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    private String title;
+    @Column(nullable = false)
+    private String description;
+    private boolean complete;
 }
