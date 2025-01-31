e8674d465dda18c15d25fb31b7e15f6d6067cc68
SirajDilshan <sirajdilshan789@gmail.com>
2025-02-01 00:01:31 +0530

    create DTOs

--- src/main/java/SpringBoot/TO_DO/dto/request/ToDoRequestDTO.java
+++ src/main/java/SpringBoot/TO_DO/dto/request/ToDoRequestDTO.java
@@ -1,4 +1,16 @@
 package SpringBoot.TO_DO.dto.request;
 
+import jakarta.persistence.Column;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class ToDoRequestDTO {
+
+    private String title;
+    private String description;
+    private boolean complete;
 }
--- src/main/java/SpringBoot/TO_DO/dto/response/ToDoResponseDTO.java
+++ src/main/java/SpringBoot/TO_DO/dto/response/ToDoResponseDTO.java
@@ -1,4 +1,15 @@
 package SpringBoot.TO_DO.dto.response;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class ToDoResponseDTO {
+    private Long id;
+    private String title;
+    private String description;
+    private boolean complete;
 }
